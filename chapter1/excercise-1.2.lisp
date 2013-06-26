da50c0f9b17be88377ca0059facc3689aec88985
tajima-junpei <tajima-junpei@tajimajunpei-ThinkPad-X60.(none)>
2013-06-26 12:10:16 +0900

    add excercise-1.2

---
+++ chapter1/excercise-1.2.lisp
@@ -0,0 +1,17 @@
+;; Write a function to exponentiate, or raise a number to an integer power. For example: (power 3 2) = 3^2 = 9
+
+(use-package :cl-test-more)
+
+(plan nil)
+
+(defun power (base n)
+  "Exponentiate, or raise a number to an integer power.
+For example: (power 3 2) = 3^2 = 9"
+  (if (= n 1)
+      base
+      (* base (power base (- n 1)))))
+
+(is (power 3 1) 3)
+(is (power 3 2) 9)
+
+(finalize)
